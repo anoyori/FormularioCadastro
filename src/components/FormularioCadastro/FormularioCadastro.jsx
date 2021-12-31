988c8ec37f98a78aec65e985148daf7b6b87070d
anoyori <anoyori@gmail.com>
2021-12-31 00:43:44 -0300

    Adicionando renderização com condicionais

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useState } from "react";
+
+import { Typography } from "@material-ui/core";
+
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+  return <>{formularioAtual(etapaAtual)}</>;
 }
+
+function formularioAtual(etapa) {
+  switch (etapa) {
+    case 0:
+      return <DadosUsuario />;
+    case 1:
+      return <DadosPessoais />;
+    case 2:
+      return <DadosEntrega />;
+    default:
+      return <Typography>Erro ao selecionar formulário</Typography>;
+  }
+}
+//
 
 export default FormularioCadastro;
